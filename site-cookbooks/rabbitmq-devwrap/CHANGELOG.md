103a075501e8c052975a682bff854cfb378ecf7d
Ionuț Arțăriși <iartarisi@suse.cz>
2013-04-24 13:13:38 +0200

    RabbitMQ user setup cookbook

---
+++ site-cookbooks/rabbitmq-devwrap/CHANGELOG.md
@@ -0,0 +1,5 @@
+# CHANGELOG for rabbitmq-devwrap
+
+## 0.1.0:
+
+* Initial release of rabbitmq-devwrap
---
+++ site-cookbooks/rabbitmq-devwrap/metadata.rb
@@ -0,0 +1,7 @@
+name             'rabbitmq-devwrap'
+maintainer       'SUSE Linux Products GmbH'
+maintainer_email 'cloud-devel@suse.de'
+license          'MIT'
+description      'Sets up RabbitMQ for a development deployment of OpenStack'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
